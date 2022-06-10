f17792b32dc010873ac37250442771dca3a5ec09
Vincent Charbonnier <vincent.charbonnier@hpe.com>
2022-06-10 09:43:51 +0200

    updated

--- Data Modeling/Project 1/sql_queries.py
+++ Data Modeling/Project 1/sql_queries.py
@@ -8,78 +8,67 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE songplays ( \
-    "songplay_id" INTEGER NOT NULL, \
-    "start_time" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, \
-    "user_id" INTEGER NOT NULL, \
-    "level" VARCHAR(255) NOT NULL, \
-    "song_id" VARCHAR(255) NOT NULL, \
-    "artist_id" VARCHAR(255) NOT NULL, \
-    "session_id" INTEGER NOT NULL, \
-    "location" VARCHAR(255) NOT NULL, \
-    "user_agent" VARCHAR(255) NOT NULL \
-); \
-""")
+songplay_table_create = ("CREATE TABLE songplays ( \
+    songplay_id serial PRIMARY KEY, \
+    start_time bigint NOT NULL, \
+    user_id int NOT NULL, \
+    level varchar NOT NULL, \
+    song_id varchar, \
+    artist_id varchar, \
+    session_id int NOT NULL, \
+    location varchar NOT NULL, \
+    user_agent varchar NOT NULL \
+)")
 
 user_table_create = ("CREATE TABLE users ( \
-    "user_id" INTEGER NOT NULL, \
-    "first_name" VARCHAR(255) NOT NULL, \
-    "last_name" VARCHAR(255) NOT NULL, \
-    "gender" CHAR(255) NOT NULL, \
-    "level" VARCHAR(255) NOT NULL \
-); \
-")
+    user_id int PRIMARY KEY, \
+    first_name varchar NOT NULL, \
+    last_name varchar NOT NULL, \
+    gender char(1) NOT NULL, \
+    level varchar NOT NULL \
+)")
 
 song_table_create = ("CREATE TABLE songs ( \
-    "song_id" VARCHAR(255) NOT NULL, \
-    "title" VARCHAR(255) NOT NULL, \
-    "artist_id" VARCHAR(255) NOT NULL, \
-    "year" INTEGER NOT NULL, \
-    "duration" DECIMAL(8, 2) NOT NULL \
-); \
-")
+    song_id varchar PRIMARY KEY, \
+    title varchar NOT NULL, \
+    artist_id varchar NOT NULL, \
+    year int NOT NULL, \
+    duration double precision NOT NULL \
+)")
 
 artist_table_create = ("CREATE TABLE artists ( \
-    "artist_id" VARCHAR(255) NOT NULL, \
-    "name" VARCHAR(255) NOT NULL, \
-    "location" VARCHAR(255) NOT NULL, \
-    "latitude" DECIMAL(8, 2) NOT NULL, \
-    "longitude" DECIMAL(8, 2) NOT NULL \
-); \
-")
+    artist_id varchar PRIMARY KEY, \
+    name varchar NOT NULL, \
+    location varchar NOT NULL, \
+    latitude decimal NOT NULL, \
+    longitude decimal NOT NULL \
+)")
 
 time_table_create = ("CREATE TABLE time ( \
-    "start_time" INTEGER NOT NULL, \
-    "hour" INTEGER NOT NULL, \
-    "day" INTEGER NOT NULL, \
-    "month" INTEGER NOT NULL, \
-    "year" INTEGER NOT NULL, \
-    "weekday" INTEGER NOT NULL \ 
-); \
-")
+    start_time bigint PRIMARY KEY, \
+    hour int NOT NULL, \
+    day int NOT NULL, \
+    week int NOT NULL, \
+    month int NOT NULL, \
+    year int NOT NULL, \
+    weekday int NOT NULL \
+)")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = ("INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
 
-user_table_insert = ("""
-""")
+user_table_insert = ("INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
 
-song_table_insert = ("""
-""")
+song_table_insert = ("INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
 
-artist_table_insert = ("""
-""")
+artist_table_insert = ("INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
 
-
-time_table_insert = ("""
-""")
+time_table_insert = ("INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES(%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING")
 
 # FIND SONGS
 
-song_select = ("""
-""")
+song_select = ("SELECT songs.song_id, artists.artist_id FROM songs JOIN artists ON songs.artist_id = artists.artist_id WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s")  
 
 # QUERY LISTS
 
